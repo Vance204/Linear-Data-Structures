8b259a35c15730ec0f631e409da0b65d2c04fd30
Vance Kerr Amor <131736201+Vance204@users.noreply.github.com>
2025-09-05 13:08:57 +0800

    Create CircularLinkedList.java

---
+++ CircularLinkedList.java
@@ -0,0 +1,47 @@
+public class CircularLinkedList {
+    Node head = null;
+    Node tail = null;
+
+
+        public void insert(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+                newNode.next = head;
+            } else{
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+        public void delete(){
+             if (head == null){
+                 System.out.println("Circular list is empty, nothing to delete.");
+                 return;
+             }
+             if(head == tail){
+                 head = null;
+                 tail = null;
+             } else{
+                 head = head.next;
+                 tail.next = head;
+             }
+                System.out.println("Deleted node...");
+            }
+            public void display() {
+                if (head == null) {
+                    System.out.println("Circular list is empty.");
+                    return;
+                }
+
+                Node current = head;
+                do {
+                    System.out.print(current.data + " ");
+                    current = current.next;
+                } while (current != head);   // stop when back at start
+
+                System.out.println();
+            }
+    }
+
